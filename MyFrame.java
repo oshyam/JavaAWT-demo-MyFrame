1f0de3d5bb0a3eb087ed1a91825d7ecae2716c8f
Shyam Mohan Kunwar <53939662+oshyam@users.noreply.github.com>
2019-11-07 20:29:22 +0530

    Added Simple Frame using Java-Awt

---
+++ MyFrame.java
@@ -0,0 +1,14 @@
+import java.applet.*;
+import java.awt.*;
+
+public class MyFrame
+{
+  public static void main(String[]args)
+  {
+    Frame frame = new Frame ("I am in frame");
+      frame.resize(500,500);
+      frame.setBackground (Color.blue);
+      frame.show ();
+      
+  }
+}
